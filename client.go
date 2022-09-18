f9641853e923a8d94840498c8ea57a8ec5dd85c8
wuhuaping2006@126.com <wuhuaping2006@126.com>
2022-09-18 11:50:48 +0800

    菜单功能

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	//2.链接服务器
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s%d", serverIp, serverPort))
@@ -41,6 +43,50 @@ func init() {
 	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口，默认值8888")
 }
 
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("请输入合法范围内的数字")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	//循环调用客户端的flag 判断是否是退出模式
+	for client.flag != 0 {
+		//输入数字非法
+		for client.menu() != true {
+
+		}
+		//根据输入的不同 处理不同的业务
+		switch client.flag {
+		case 1:
+			//公聊模式
+			fmt.Println("公聊模式...")
+			break
+		case 2:
+			//私聊模式
+			fmt.Println("私聊模式...")
+			break
+		case 3:
+			//更新用户名
+			fmt.Println("更新用户名...")
+			break
+
+		}
+	}
+}
+
 func main() {
 	//命令行解析
 	flag.Parse()
@@ -53,5 +99,6 @@ func main() {
 	fmt.Println("链接服务器成功")
 
 	//启动客户端业务
-	select {}
+	// select {}
+	client.Run()
 }
